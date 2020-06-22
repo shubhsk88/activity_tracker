e19bdebf3f2df42d33ef4d36bf1e650f58f0af57
shubhsk88 <shubham88singh@gmail.com>
2020-06-22 22:20:05 +0530

    Adding the session_controlls method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+
+    def new
+        @session=Session.new
+
+        
+    end
+
+    def create
+        @session=Session.new(session_params)   
+        @session.author_id=current_user.id
+        if(@session.save)
+            redirect_to user_path,notice:"Session sucessfully Created"
+        else
+        render :new
+        end
+    end
+
+
+    private
+
+    def session_params
+        params.require(:session).permit(:name,:amount)
+    end
 end
