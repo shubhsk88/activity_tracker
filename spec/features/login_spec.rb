38439d40cf744968e7eaf4c4e59d3a926dbd7ef5
shubhsk88 <shubham88singh@gmail.com>
2020-06-25 20:44:26 +0530

    Fixing the login spec test case

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-RSpec.describe 'Login page ', type: :feature do
+RSpec.describe 'login followed by logout', type: :feature do
   # The user must exist so we are creating it here
-  let(:user) { User.create(name: 'Shubham Singh', username: 'sgubp') }
+  let!(:user) { User.create(name: 'Shubham Singh', username: 'sgubp') }
 
   scenario 'login page' do
     visit '/signin'
@@ -13,6 +13,8 @@ RSpec.describe 'Login page ', type: :feature do
     sleep(3)
     expect(page).to have_content(user.name)
 
-    
+    click_on 'Log Out'
+    sleep(3)
+    expect(page). to have_text('Log In')
   end
 end
